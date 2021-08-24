130446517f7e994a8775ac4a5527e852aa3fa697
jmp2017 <45702686+JohnPandi@users.noreply.github.com>
2021-08-24 15:34:12 +1000

    Update Startup.cs

--- CourtCaseManagement.Api/Startup.cs
+++ CourtCaseManagement.Api/Startup.cs
@@ -38,7 +38,7 @@ namespace CourtCaseManagement.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+        //    app.UseHttpsRedirection();
 
             app.UseRouting();
 
